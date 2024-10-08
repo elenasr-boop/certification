34dcc730132886b32173f55a4cb16eed9d85c5e8
Elena Rybakova <lelkya84@gmail.com>
2024-10-08 17:46:09 +0300

    апи перенесла

--- src/api.js
+++ src/api.js
@@ -1,5 +1,24 @@
-export async function getUsers (login) {
-    const res = await fetch(`https://api.github.com/search/users?q=${login}`);
-    const data = await res.json();
-    return data;
+import { Octokit } from "@octokit/core";
+
+const octokit = new Octokit({
+  auth: "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",
+});
+
+export async function getUsers(request, isIncrease) {
+    const res = await octokit.request(`GET /search/users?q=${request};sort=repositories;order=${isIncrease ? "asc" : "desc"}`, {
+        headers: {
+          "X-GitHub-Api-Version": "2022-11-28",
+        },
+        per_page: 50,
+      });
+    return {data: res.data, links: res.headers.link};
+}
+
+export async function changePage(link) {
+  const res = await octokit.request(`GET ${link}`, {
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    }
+  });
+  return {data: res.data, links: res.headers.link};
 }
